c715401cb678c8d9a7ddf6a692ecc5e2abc61b35
4K45H107 <fahad11.ahmed11@gmail.com>
2024-10-20 05:34:15 +0600

    vercel setup 5

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
-  console.log(`Notebook backend listening on port http://localhost:${port}`);
+  console.log(`Notebook backend listening`);
 });
